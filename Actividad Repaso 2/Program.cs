043ebe573886c615685c668cbb0ed1afb7a99557
joseagustindelacruzrd-ui <joseagustindelacruzrd@gmail.com>
2026-04-02 22:39:30 -0400

    actividad repaso 2
    actv repaso 2

--- Actividad Repaso 2/Program.cs
+++ Actividad Repaso 2/Program.cs
@@ -1,1 +1,37 @@
-Actividad Repaso 2
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int capacidad = 20;
+
+        Console.WriteLine("=== AUTOBUS PLATINUM ===");
+
+        Console.Write("Cantidad de pasajeros: ");
+        int pasajeros = int.Parse(Console.ReadLine());
+
+        int precio = 1000; // puedes cambiarlo
+        int ventas = pasajeros * precio;
+
+        int asientosDisponibles = capacidad - pasajeros;
+
+        Console.WriteLine($"Auto Bus Platinum {pasajeros} Pasajeros");
+        Console.WriteLine($"Ventas {ventas}");
+        Console.WriteLine($"Quedan {asientosDisponibles} asientos disponibles");
+
+        Console.WriteLine("\n=== AUTOBUS GOLD ===");
+
+        Console.Write("Cantidad de pasajeros: ");
+        pasajeros = int.Parse(Console.ReadLine());
+
+        precio = 800; // otro precio
+        ventas = pasajeros * precio;
+
+        asientosDisponibles = capacidad - pasajeros;
+
+        Console.WriteLine($"Auto Bus Gold {pasajeros} Pasajeros");
+        Console.WriteLine($"Ventas {ventas}");
+        Console.WriteLine($"Quedan {asientosDisponibles} asientos disponibles");
+    }
+}
